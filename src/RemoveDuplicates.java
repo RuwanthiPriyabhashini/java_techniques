08517bad612eb613ea212fef6c89d8a1ea3dfd43
RuwanthiPriyabhashini <ruwanthipriyabhashini1996@gmail.com>
2023-02-22 23:49:37 +0530

    commited

--- src/RemoveDuplicates.java
+++ src/RemoveDuplicates.java
@@ -1,3 +1,16 @@
+import java.util.Arrays;
+
 public class RemoveDuplicates {
-    
+    public static void main(String args[]){
+        int[] arr = {2,3,2,4,3,5,6,5,7,8,8};
+
+        //sort the array to bring duplicates together
+
+        Arrays.sort(arr);
+
+        for (int i=0;i< arr.length;i++){
+            System.out.print(arr[i]);
+        }
+
+    }
 }
