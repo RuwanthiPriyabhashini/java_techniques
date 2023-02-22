1e2ed58c1bde5742edee518b020fc26beb3b90d1
RuwanthiPriyabhashini <ruwanthipriyabhashini1996@gmail.com>
2023-02-23 00:01:04 +0530

    commited

--- src/RemoveDuplicates.java
+++ src/RemoveDuplicates.java
@@ -5,12 +5,29 @@ public class RemoveDuplicates {
         int[] arr = {2,3,2,4,3,5,6,5,7,8,8};
 
         //sort the array to bring duplicates together
-
         Arrays.sort(arr);
 
-        for (int i=0;i< arr.length;i++){
-            System.out.print(arr[i]);
+       //create new array to store unique values
+        int[] newArr =new int [arr.length];
+        int j=0;
+
+        //loop through the sored array, skipping duplicates
+        for(int i=0;i<arr.length-1;i++){
+            if(arr[i] != arr[i+1]){
+                newArr[j++] =arr[i];
+            }
         }
+
+        //add the last element to the new array
+        newArr[j++] =arr[arr.length-1];
+
+
+        //create final array with only the unique values
+        int[] finalArr =new int[j];
+        for(int i=0;i<j;i++){
+            finalArr[i] =newArr[i];
+        }
+        System.out.println(Arrays.toString(finalArr));
 
     }
 }
