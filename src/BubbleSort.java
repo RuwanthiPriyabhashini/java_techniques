24916f00db583ee51dced225bc137259d0721f16
RuwanthiPriyabhashini <ruwanthipriyabhashini1996@gmail.com>
2023-02-23 00:35:57 +0530

    commited..

--- src/BubbleSort.java
+++ src/BubbleSort.java
@@ -1,23 +1,18 @@
-import java.sql.SQLOutput;
-import java.util.Arrays;
-
 public class BubbleSort {
-    public static void main(String args[]){
-        int arr[] = {2 , 5,  1 , 7 , 9};
+    public static void main (String args[]){
+        int arr[] = {4 , 1 , 7 , 4 , 9 };
 
-        for(int i=0;i< arr.length-1;i++){
-            for(int j=0;j< arr.length - i- 1;j++){
+        for(int i=0; i< arr.length - 1; i++){
+            for(int j=0;j< arr.length -i -1;j++){
                 if(arr[j] > arr[j+1]){
                     int temp = arr[j];
                     arr[j] = arr[j+1];
-                    arr[j+1]= temp;
+                    arr[j+1]=temp;
                 }
             }
-        }
+            }
         for(int i=0;i< arr.length;i++){
-            System.out.print(arr[i] +" , ");
+            System.out.print(arr[i] + " ");
         }
-
-
     }
 }
